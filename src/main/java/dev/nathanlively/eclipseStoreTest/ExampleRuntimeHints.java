66d0e371ed3b55aa11ab44d44d232cd0b44323fc
Nathan Lively <60303904+LiveNathan@users.noreply.github.com>
2024-10-25 11:25:38 -0500

    Update runtime hints and dependencies
    Enhanced runtime reflection hints in `ExampleRuntimeHints` by correcting the package import for `Optional`. Upgraded Spring Boot to version 3.3.5 and updated Eclipse store dependency to version 2.0.0 in `pom.xml`. Removed unnecessary license, developer, and source control management tags from `pom.xml`.

--- src/main/java/dev/nathanlively/eclipseStoreTest/ExampleRuntimeHints.java
+++ src/main/java/dev/nathanlively/eclipseStoreTest/ExampleRuntimeHints.java
@@ -1,6 +1,7 @@
 package dev.nathanlively.eclipseStoreTest;
 
 import dev.nathanlively.eclipseStoreTest.adapter.out.eclipse.EclipseAccountAdapter;
+import org.apache.el.stream.Optional;
 import org.eclipse.serializer.collections.EqHashEnum;
 import org.eclipse.store.integrations.spring.boot.types.factories.EmbeddedStorageFoundationFactory;
 import org.eclipse.store.storage.embedded.types.EmbeddedStorageFoundation;
@@ -8,8 +9,6 @@ import org.eclipse.store.storage.embedded.types.EmbeddedStorageManager;
 import org.springframework.aot.hint.MemberCategory;
 import org.springframework.aot.hint.RuntimeHints;
 import org.springframework.aot.hint.RuntimeHintsRegistrar;
-
-import java.util.Optional;
 
 public class ExampleRuntimeHints implements RuntimeHintsRegistrar {
     @Override
@@ -27,4 +26,5 @@ public class ExampleRuntimeHints implements RuntimeHintsRegistrar {
         hints.reflection().registerType(Optional.class, MemberCategory.PUBLIC_FIELDS, MemberCategory.INVOKE_PUBLIC_METHODS);
         hints.reflection().registerType(EqHashEnum.class, MemberCategory.PUBLIC_FIELDS, MemberCategory.DECLARED_FIELDS, MemberCategory.INVOKE_PUBLIC_CONSTRUCTORS, MemberCategory.INVOKE_PUBLIC_METHODS, MemberCategory.INVOKE_DECLARED_METHODS);
     }
+
 }
